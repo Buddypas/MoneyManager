f51d696e6e034c6ecdd5efccbbf529d8375f40af
Nikola Ivanovic <nikola.ivanovic@amplitudo.me>
2021-03-01 17:46:21 +0100

    Initial commit

---
+++ app/src/main/java/com/inFlow/moneyManager/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.inFlow.moneyManager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
