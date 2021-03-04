1c5c39ad87240d49ac61860e5ab9346a78435f78
Nikola Ivanovic <nikola.ivanovic@amplitudo.me>
2021-03-04 22:43:33 +0100

    Working on dashboard UI

--- app/src/main/java/com/inFlow/moneyManager/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/inFlow/moneyManager/ui/dashboard/DashboardFragment.kt
@@ -6,17 +6,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import com.inFlow.moneyManager.R
+import com.inFlow.moneyManager.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
-    private lateinit var viewModel: DashboardViewModel
+    private var _binding: FragmentDashboardBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    ): View {
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val items = listOf("Material", "Design", "Components", "Android")
+        val adapter = ArrayAdapter(requireContext(), R.layout.item_wallet_dropdown, items)
+        binding.walletDropdown.setAdapter(adapter)
+        binding.walletDropdown.setText(adapter.getItem(0).toString(), false)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
