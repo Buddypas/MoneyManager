3417ff2bb0c4dbb565be19f02ce4a4356bed3224
Nikola Ivanovic <nikola.ivanovic@amplitudo.me>
2021-03-13 18:01:25 +0100

    Encountered date validation error

--- app/src/main/java/com/inFlow/moneyManager/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/inFlow/moneyManager/ui/dashboard/DashboardFragment.kt
@@ -7,10 +7,15 @@ import android.widget.ArrayAdapter
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.fragment.findNavController
 import com.inFlow.moneyManager.R
 import com.inFlow.moneyManager.databinding.FragmentDashboardBinding
+import com.inFlow.moneyManager.shared.kotlin.KEY_FILTERS
 import com.inFlow.moneyManager.shared.kotlin.onQueryTextChanged
+import com.inFlow.moneyManager.ui.filters.PeriodMode
+import com.inFlow.moneyManager.vo.FiltersDto
 import org.koin.android.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
@@ -29,9 +34,47 @@ class DashboardFragment : Fragment() {
         return binding.root
     }
 
+    private fun setNavObserver() {
+        val navController = findNavController()
+        val navBackStackEntry = navController.getBackStackEntry(R.id.dashboardFragment)
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_RESUME
+                && navBackStackEntry.savedStateHandle.contains(KEY_FILTERS)
+            ) {
+                val result = navBackStackEntry.savedStateHandle.get<FiltersDto>(KEY_FILTERS)
+                formatFilters(result)
+            }
+        }
+        navBackStackEntry.lifecycle.addObserver(observer)
+
+        viewLifecycleOwner.lifecycle.addObserver(LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_DESTROY) {
+                navBackStackEntry.lifecycle.removeObserver(observer)
+            }
+        })
+    }
+
+    private fun formatFilters(data: FiltersDto?) {
+        data?.let {
+            val argType = when {
+                it.showExpenses && it.showIncomes -> "all transactions"
+                it.showExpenses -> "all expenses"
+                else -> "all incomes"
+            }
+            val argPeriod =
+                if (it.period == PeriodMode.WHOLE_MONTH) "in ${it.fromDate?.month?.name} of ${it.fromDate?.year}"
+                else "from ${it.fromDate} to ${it.toDate}"
+            val argSort = it.sortBy
+            val argOrder = if (it.isDescending) "descending" else "ascending"
+            binding.filtersTxt.text =
+                getString(R.string.filters_template, argType, argPeriod, argSort, argOrder)
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setNavObserver()
         val wallets = listOf("Material", "Design", "Components", "Android")
         val walletAdapter = ArrayAdapter(requireContext(), R.layout.item_wallet_dropdown, wallets)
         binding.walletDropdown.setAdapter(walletAdapter)
@@ -44,15 +87,19 @@ class DashboardFragment : Fragment() {
             viewModel.searchQuery.value = it
         }
         binding.toolbar.setOnMenuItemClickListener { menuItem ->
-            when(menuItem.itemId) {
+            when (menuItem.itemId) {
                 R.id.action_filter -> {
-                    findNavController().navigate(DashboardFragmentDirections.actionDashboardToFilters(viewModel.activeFilters.value))
+                    findNavController().navigate(
+                        DashboardFragmentDirections.actionDashboardToFilters(
+                            viewModel.activeFilters.value
+                        )
+                    )
                     true
                 }
                 else -> false
             }
         }
-
+        formatFilters(FiltersDto())
     }
 
     override fun onDestroyView() {
