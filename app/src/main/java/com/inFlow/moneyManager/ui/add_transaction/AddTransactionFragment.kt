45f8d6743b3cc9f733aa14df5b757c88ac5f17bf
Nikola Ivanovic <nikola.ivanovic@amplitudo.me>
2021-04-25 21:08:30 +0200

    Moved logic to fragment

--- app/src/main/java/com/inFlow/moneyManager/ui/add_transaction/AddTransactionFragment.kt
+++ app/src/main/java/com/inFlow/moneyManager/ui/add_transaction/AddTransactionFragment.kt
@@ -1,23 +1,19 @@
 package com.inFlow.moneyManager.ui.add_transaction
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.text.SpannableString
 import android.text.SpannableStringBuilder
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.lifecycle.lifecycleScope
 import com.inFlow.moneyManager.R
 import com.inFlow.moneyManager.databinding.FragmentAddTransactionBinding
-import com.inFlow.moneyManager.databinding.FragmentDashboardBinding
 import com.inFlow.moneyManager.db.entities.Category
 import com.inFlow.moneyManager.shared.kotlin.onSelectedItemChanged
+import com.inFlow.moneyManager.shared.kotlin.setValueIfDifferent
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 import org.koin.android.viewmodel.ext.android.viewModel
 
 /**
@@ -26,15 +22,19 @@ import org.koin.android.viewmodel.ext.android.viewModel
  * 3. Set initial category to be expense
  */
 class AddTransactionFragment : Fragment() {
-
     private var _binding: FragmentAddTransactionBinding? = null
     private val binding get() = _binding!!
     private val viewModel: AddTransactionViewModel by viewModel()
+
+    private var categoryType = CategoryType.EXPENSE
+    private var selectedCategoryPosition = -1
 
     private var categoryAdapter: ArrayAdapter<String>? = null
 
     val incomeList = mutableListOf<Category>()
     val expenseList = mutableListOf<Category>()
+
+//    val activeCategoryList = mutableListOf<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,45 +47,58 @@ class AddTransactionFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initDropdownAdapter()
-
-        binding.typeRadioGroup.setOnCheckedChangeListener { _, checkedId ->
-            viewModel.updateCategoryPosition(null)
+        binding.expenseBtn.setOnCheckedChangeListener { buttonView, isChecked ->
+            selectedCategoryPosition = -1
             binding.categoryDropdown.text = SpannableStringBuilder("")
-            viewModel.onTypeSelected(checkedId)
+            onTypeSelected(isChecked)
         }
+
+//        binding.typeRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+//            selectedCategoryPosition = -1
+//            binding.categoryDropdown.text = SpannableStringBuilder("")
+//            onTypeSelected(checkedId)
+//        }
 
         binding.categoryDropdown.onSelectedItemChanged {
-            viewModel.updateCategoryPosition(it)
-        }
-
-        lifecycleScope.launchWhenStarted {
-            viewModel.categoryType.collectLatest { t ->
-                t?.let { type ->
-                    categoryAdapter?.clear()
-                    if (type == CategoryType.EXPENSE) categoryAdapter?.addAll(expenseList.map { it.categoryName })
-                    else categoryAdapter?.addAll(incomeList.map { it.categoryName })
-                    categoryAdapter?.notifyDataSetChanged()
-                }
-            }
+            selectedCategoryPosition = it
         }
 
         with(viewModel) {
             incomes.observe(viewLifecycleOwner, { incomeList.addAll(it) })
             expenses.observe(viewLifecycleOwner, { expenseList.addAll(it) })
-//            categoriesReady.observe(viewLifecycleOwner, {
-//                if (it) initDropdownAdapter()
-//            })
+            categoriesReady.observe(viewLifecycleOwner, {
+                if (it) initDropdownAdapter()
+            })
             initData()
         }
     }
 
+    private fun onTypeSelected(isExpenseChecked: Boolean) {
+        categoryType =
+            if (isExpenseChecked) CategoryType.EXPENSE
+            else CategoryType.INCOME
+        val list =
+            if (categoryType == CategoryType.EXPENSE)
+                expenseList.map { it.categoryName }
+            else
+                incomeList.map { it.categoryName }
+        categoryAdapter?.apply {
+            clear()
+            addAll(list)
+//            notifyDataSetChanged()
+        }
+    }
+
     private fun initDropdownAdapter() {
-        if (categoryAdapter == null)
+        if (categoryAdapter == null) {
+            val list = expenseList.map { it.categoryName }
             categoryAdapter = ArrayAdapter(
                 requireContext(),
                 R.layout.item_month_dropdown,
-                expenseList.map { it.categoryName })
-                .also { binding.categoryDropdown.setAdapter(it) }
+                R.id.dropdown_txt,
+                list
+            ).apply { setNotifyOnChange(true) }
+            binding.categoryDropdown.setAdapter(categoryAdapter)
+        }
     }
 }
--- app/src/main/java/com/inFlow/moneyManager/ui/add_transaction/AddTransactionViewModel.kt
+++ app/src/main/java/com/inFlow/moneyManager/ui/add_transaction/AddTransactionViewModel.kt
@@ -8,15 +8,11 @@ import com.inFlow.moneyManager.R
 import com.inFlow.moneyManager.db.entities.Category
 import com.inFlow.moneyManager.repository.AppRepository
 import com.inFlow.moneyManager.shared.kotlin.setValueIfDifferent
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class AddTransactionViewModel(private val repository: AppRepository) : ViewModel() {
-    private val _categoryType = MutableStateFlow<CategoryType?>(null)
-    val categoryType: StateFlow<CategoryType?> by this::_categoryType
-
-    val _selectedCategoryPosition = MutableStateFlow<Int?>(null)
-
     private val _expenseFlow = MutableStateFlow(listOf<Category>())
     val expenseFlow: StateFlow<List<Category>> by this::_expenseFlow
 
@@ -29,14 +25,6 @@ class AddTransactionViewModel(private val repository: AppRepository) : ViewModel
     val categoriesReady = MediatorLiveData<Boolean>()
 
     init {
-//        viewModelScope.launch {
-//            repository.db.categoriesDao().insertAll(
-//                Category(categoryId = 1,categoryName = "Gorivo", categoryType = "expense"),
-//                Category(categoryId = 2,categoryName = "Plata", categoryType = "income"),
-//                Category(categoryId = 3,categoryName = "Doktor", categoryType = "expense"),
-//                Category(categoryId = 4,categoryName = "Kladionica", categoryType = "income")
-//            )
-//        }
         categoriesReady.value = false
         categoriesReady.addSource(expenses) {
             categoriesReady.value = it.isNotEmpty() && !incomes.value.isNullOrEmpty()
@@ -49,29 +37,21 @@ class AddTransactionViewModel(private val repository: AppRepository) : ViewModel
     fun initData() = viewModelScope.launch {
         loadExpenses()
         loadIncomes()
-        _categoryType.setValueIfDifferent(CategoryType.EXPENSE)
     }
 
-    fun updateCategoryPosition(position: Int? = null) {
-        _selectedCategoryPosition.setValueIfDifferent(position)
-    }
-
-    fun onTypeSelected(checkedId: Int) {
-        if (checkedId == R.id.expense_btn)
-            _categoryType.setValueIfDifferent(CategoryType.EXPENSE)
-        else
-            _categoryType.setValueIfDifferent(CategoryType.INCOME)
-    }
-
-    fun loadExpenses() = viewModelScope.launch {
+    private fun loadExpenses() = viewModelScope.launch {
         _expenseFlow.emitAll(repository.getAllExpenseCategories())
     }
 
-    fun loadIncomes() = viewModelScope.launch {
+    private fun loadIncomes() = viewModelScope.launch {
         _incomeFlow.emitAll(repository.getAllIncomeCategories())
     }
 }
 
 enum class CategoryType {
     EXPENSE, INCOME;
+}
+
+sealed class AddTransactionEvent {
+    data class CategoryTypeChanged(val categoryType: CategoryType) : AddTransactionEvent()
 }
