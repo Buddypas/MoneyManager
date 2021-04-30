14de704261527419b591ec9e2b02805f438ed665
Nikola Ivanovic <nikola.ivanovic@amplitudo.me>
2021-04-30 16:59:28 +0200

    Added app bar to categories

--- app/src/main/java/com/inFlow/moneyManager/ui/categories/CategoriesViewModel.kt
+++ app/src/main/java/com/inFlow/moneyManager/ui/categories/CategoriesViewModel.kt
@@ -5,9 +5,5 @@ import androidx.lifecycle.asLiveData
 import com.inFlow.moneyManager.repository.AppRepository
 
 class CategoriesViewModel(private val repository: AppRepository) : ViewModel() {
-
     val categoryList = repository.getAllCategories()
-
-//    val categories = categoryList.asLiveData()
-
 }
