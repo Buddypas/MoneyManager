81d3457d5c276ddce6950fb9b36d55b4adadc1af
Nikola Ivanovic <nikola.ivanovic@amplitudo.me>
2021-06-09 20:23:16 +0200

    Imported hilt

--- app/src/main/java/com/inFlow/moneyManager/BaseApp.kt
+++ app/src/main/java/com/inFlow/moneyManager/BaseApp.kt
@@ -3,12 +3,14 @@ package com.inFlow.moneyManager
 import android.app.Application
 import com.inFlow.moneyManager.di.dataModule
 import com.inFlow.moneyManager.di.viewModelModule
+import dagger.hilt.android.HiltAndroidApp
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import timber.log.Timber
 
+//@HiltAndroidApp
 class BaseApp : Application() {
     override fun onCreate() {
         super.onCreate()
